9e4757e2fe782d410c8cb3af8f16f1e31ccc27b8
Olivier Leduc <leduc.o@gmail.com>
2020-05-16 22:43:12 -0400

    feat: :sparkles: Minimum value
    User can now specify a minimum of value to roll. This is done using `>n`.

--- dice.py
+++ dice.py
@@ -6,7 +6,9 @@ import random
 
 from colorama import  Fore
 
-PATTERN = re.compile(r'\s*(?P<nb>\d+)d(?P<sides>\d+)\s*(?:(?P<mod>[\+-])\s*(?P<val>\d+))?')
+PATTERN = re.compile(
+  r'\s*(?P<nb>\d+)d(?P<sides>\d+)(?:>(?P<min>\d+))?\s*(?:(?P<mod>[\+-])\s*(?P<val>\d+))?'
+)
 WELCOME_MSG = r"""
  ______     _                   ______    _                        __          _                   
 |_   _ `.  (_)                .' ____ \  (_)                      [  |        / |_                 
@@ -20,10 +22,11 @@ WELCOME_MSG = r"""
 """
 
 HELP_MSG = r"""
-Usage: [nb]d[sides](+-)[mod]
+Usage: [nb]d[sides]>[min] (+-) [mod]
   nb        number of dices
   sides     number of sides
   mod       result modifier (+ or -)
+  min       minium value of roll
 
 Special command:
   q, quit, exit   quit the app
@@ -33,38 +36,42 @@ Special command:
   help            show this screen
   
 Example: 
-> 1d6 + 1
-( 3 ) + 1 = 4
-> 3d4
-( 1 + 3 + 2 ) = 6
+  > 1d6 + 1
+  ( 3 ) + 1 = 4
+  > 3d4
+  ( 1 + 3 + 2 ) = 6
 """
 
 def read_input(text):
   try:
     m = PATTERN.match(text).groupdict()
-    dices = make_dices(int(m['nb']), int(m['sides']))
     mod = m['mod'] or '+'
     val = m['val'] or 0
     val = int(val)
-
+    minimum = m['min'] or 1
     if mod == '-':
       val *= -1
-  except Exception:
+
+    dices = make_dices(int(m['nb']), int(m['sides']), int(minimum))
+  except Exception as e:
     raise ValueError("Can't process the input.")
   return Hand(dices, val)
 
 
-def make_dices(nb, sides):
-  return [Dice(sides) for _ in range(nb)]
+def make_dices(nb, sides, minimum):
+  if minimum > sides:
+    raise ValueError("Minimum can't be greater than the number of side.")
+  return [Dice(sides, minimum) for _ in range(nb)]
 
 
 class Dice(object):
-  def __init__(self, sides):
+  def __init__(self, sides, minimum=1):
     self.sides = sides
     self.res = None
+    self.min = minimum
 
   def roll(self):
-    self.res = random.randint(1,self.sides)
+    self.res =  random.randint(self.min,self.sides)
 
   def __str__(self):
     p = str(self.res) or 'NA'
